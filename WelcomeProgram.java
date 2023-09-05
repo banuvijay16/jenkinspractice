fe66dd578e551867b35c9deb1959159523ae1fe6
banuvijay16 <136794483+banuvijay16@users.noreply.github.com>
2023-09-05 15:51:15 +0530

    Update WelcomeProgram.java

--- WelcomeProgram.java
+++ WelcomeProgram.java
@@ -3,5 +3,6 @@ public static void main(String[] args){
 System.out.println("hello");
 System.out.println("welcome to GITHUB");
 System.out.println("changes done");
+System.out.println("no output");
 }
 }
